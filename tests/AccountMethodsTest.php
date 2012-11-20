eed5be879bee025d915b92f8e7855143835b9ae1
Shawn Smith <shawnpsmith@gmail.com>
2012-11-20 16:41:34 +0900

    add account stats test

--- tests/AccountMethodsTest.php
+++ tests/AccountMethodsTest.php
@@ -26,4 +26,19 @@ class AccountMethodsTest extends PHPUnit_Framework_TestCase
         $this->assertTrue(isset($response['response']['credits']));
 
     }
+
+    public function test_get_stats()
+    {
+        $service = Gengo_Api::factory('account', $this->key, $this->secret);
+        $service->setBaseUrl('http://sandbox.gengo.com/v2/');
+
+        $service->getStats();
+        $body = $service->getResponseBody();
+        $response = json_decode($body, true);
+        $this->assertEquals($response['opstat'], 'ok');
+        $this->assertTrue(isset($response['response']));
+        $this->assertTrue(isset($response['response']['credits_spent']));
+        $this->assertTrue(isset($response['response']['user_since']));
+
+    }
 }
