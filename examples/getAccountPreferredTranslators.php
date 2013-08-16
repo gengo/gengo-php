214c7c6797f6d0e31ef10f69d8750247aabe49b4
alexlide <alex.lide@hotmail.com>
2013-08-16 15:12:01 +0900

    corrected comment

--- examples/getAccountPreferredTranslators.php
+++ examples/getAccountPreferredTranslators.php
@@ -8,7 +8,7 @@ require_once '../init.php';
 
 // TODO: this example assumes you set the 2 values below.
 $api_key = 'your-public-api-key';
-$private_key = 'our-private-api-key';
+$private_key = 'your-private-api-key';
 
 // Get an instance of an Account Client
 $account = Gengo_Api::factory('account', $api_key, $private_key);
